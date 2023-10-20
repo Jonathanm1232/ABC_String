b2e4b72ff4ac9beee8be6c59b1016ecebb697470
Jonathan Meineke <jonathan.meineke@conlance.de>
2023-10-20 10:13:48 +0200

    Added the thing to do the thing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import javax.swing.*;
+import java.lang.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input = JOptionPane.showInputDialog("Enter a string to match: ");
+        System.out.println(With.Contains(input));
+        System.out.println(With.Something(input));
+
     }
 }
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    String input = "wurschtsalatbeschteck";
+    @Test
+    void addition() {
+        assertEquals(, With.Contains(input));
+    }
+
+}
